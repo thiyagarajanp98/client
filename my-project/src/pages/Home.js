dfc5feaf1e6b60a71d3cd106055e2536f81cace2
thiyagarajanp98 <thiyagarajanp98@gmail.com>
2022-07-21 16:32:09 +0530

    1 file modified

--- my-project/src/pages/Home.js
+++ my-project/src/pages/Home.js
@@ -2,8 +2,224 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className="mb-8 w-screen h-screen md:w-screens p-8 bg-gray-200 overflow-y-scroll">
+    <div className="mb-8 w-screen  md:w-screens p-8">
       <h1 className="text-2xl text-center font-semibold">Home page</h1>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
+      <p>
+        I'm using google-login package. I've tried to update the background
+        image and dimension of the button, it doesn't seem to work or Am i doing
+        it wrong? I can't find any examples in the docs on how to implement
+        inline styling. I just read that it is an object, here is my code.
+      </p>
       <p>
         I'm using google-login package. I've tried to update the background
         image and dimension of the button, it doesn't seem to work or Am i doing
